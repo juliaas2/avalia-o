ec7781f10ce7470bce0beb865e1a5db95af242f1
juliaas2 <juliaas@al.insper.edu.br>
2024-09-22 18:06:45 -0300

    amo viver

---
+++ src/test/java/br/insper/avaliacao/AvaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.insper.avaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
